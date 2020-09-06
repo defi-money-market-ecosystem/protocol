f78632d0a95ebbdcb73f543c9f716dfd9498ed0f
Corey Caplan <coreycaplan3@gmail.com>
2020-09-06 14:04:45 -0400

    Added staking skeleton to repository; time to begin coordination

---
+++ contracts/external/staking/DMGStakingProxy.sol
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2020 DMM Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+pragma solidity ^0.5.0;
+
+import "../../../node_modules/@openzeppelin/upgrades/contracts/upgradeability/AdminUpgradeabilityProxy.sol";
+
+import "./v1/IDMGStakingV1Initializable.sol";
+
+contract DMGStakingProxy is AdminUpgradeabilityProxy {
+
+    /**
+     * @param logic                 The address of the initial implementation.
+     * @param admin                 The address of the proxy administrator.
+     */
+    constructor(
+        address logic,
+        address admin
+    )
+    AdminUpgradeabilityProxy(
+        logic,
+        admin,
+        abi.encodePacked(
+            IDMGStakingV1Initializable(address(0)).initialize.selector,
+            abi.encode("")
+        )
+    )
+    public {}
+
+    function getImplementation() external view returns (address) {
+        return _implementation();
+    }
+
+    function _willFallback() internal {
+        // Don't call super. We want the admin to be able to call-through to the implementation contract
+    }
+
+}
